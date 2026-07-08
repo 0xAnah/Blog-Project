37bccbdbf21910f53cebb6bb7868fe0fa7594e9f
0xAnah <emartu40@gmail.com>
2026-07-08 21:26:38 +0100

    feat: built the views and templates for post list and post detail

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,18 @@ from django.conf import settings
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISH)
+
+
 class Post(models.Model):
+    # text choices
     class Status(models.TextChoices):
         DRAFT = 'DR', 'Draft'
         PUBLISH = 'PR', 'PUBLISH'
-
+    
+    # models attributes
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -17,6 +24,10 @@ class Post(models.Model):
     updated = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+
+    # model managers
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ['-publish']
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    context = {'posts':posts}
+
+    return render(request, 'blog/post/list.html', context=context)
+
+def post_detail(request, id):
+
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("Post not found")
+
+    context = {'post':post}
+    return render(request, 'blog/post/detail.html', context=context)
