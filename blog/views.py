fff79e7d2991b1609e5d73a64a8e8a8f9c70268a
0xAnah <emartu40@gmail.com>
2026-07-09 20:07:33 +0100

    update: made the post url seo friendly

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 
@@ -10,12 +10,10 @@ def post_list(request):
 
     return render(request, 'blog/post/list.html', context=context)
 
-def post_detail(request, id):
-
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Post not found")
-
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post, status=Post.Status.PUBLISH, publish__year=year, publish__month=month, publish__day=day, slug=post
+    )
+    
     context = {'post':post}
     return render(request, 'blog/post/detail.html', context=context)
