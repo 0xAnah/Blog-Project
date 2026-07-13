48945540802b14b3fec59427cb5331e0bc239182
0xAnah <emartu40@gmail.com>
2026-07-13 11:26:14 +0100

    feat: Implemented post sharing via email

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
 
@@ -40,3 +42,24 @@ def post_detail(request, year, month, day, post):
     
     context = {'post':post}
     return render(request, 'blog/post/detail.html', context=context)
+
+def post_share(request, post_id):
+
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISH)
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form field pass validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} {cd['email']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comment']}"
+            send_mail(subject=subject, message=message, from_email=None, recipient_list=[cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    
+    context = {'post':post, 'form':form, 'sent':sent}
+    return render(request, 'blog/post/share.html', context=context)        
